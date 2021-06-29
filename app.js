12e6a3ef73eac21fef288bc6c1828ef8449ee183
Meshari Aldoukhi <mashry151@hotmail.com>
2021-06-29 18:55:04 +0300

    Added Delete

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* import express from "express" */
 const express = require('express')
-const gloves = require("./data")
+let gloves = require("./data")
 const cors = require("cors"); 
 
 const app = express();
@@ -13,6 +13,18 @@ app.get("/gloves", (req, res) => {
     // JSON = JavaScript Object Notation
     res.json(gloves);
   });
+
+  /* Delete  */
+app.delete("/gloves/:productId", (req, res) => {
+    const { productId } = req.params;
+    const foundProduct = gloves.find(glove => glove.id === +productId)
+    if (foundProduct){
+     gloves = gloves.filter(glove => glove.id !== +productId)
+     res.status(204).end()
+    }else {
+     res.status(404).json({message: "Not Found"})
+    }
+})
 
 /* Fine to add 9000 instead of 8000 */
   app.listen(8000, () => {
